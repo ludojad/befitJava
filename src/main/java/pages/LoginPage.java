a66c134a6345e64ecad2107e7744bd36ed8ef363
Przemyslaw Slodkiewicz <p.slodkiewicz@gmail.com>
2015-04-06 11:33:21 +0200

    Przymiarka do pageObject

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by przem on 2015-04-05.
+ */
+public class LoginPage {
+    private final WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver=driver;
+    }
+
+}
